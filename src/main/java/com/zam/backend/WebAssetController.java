be0a876fc1463812e709517f9d72f18d90668fcb
Matteo Forlani <matteo@matthew5pl.net>
2025-03-05 09:58:59 +0100

    rinominata classe WebAssetRepository in WebAssetController

--- src/main/java/com/zam/backend/WebAssetController.java
+++ src/main/java/com/zam/backend/WebAssetController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
-public class WebAssetRepository {
+public class WebAssetController {
     private final ZamAssetRepository assetRepository;
 
     @PostMapping(value = "/api/assets/all", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -17,10 +17,11 @@ public class WebAssetRepository {
     @PostMapping(value = "/api/assets/floor", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public Iterable<ZamAsset> getFloorAssets(@RequestBody ZamFloorRequest floor) {
+        ZamLogger.log("Loading floor " + floor.floor());
         return assetRepository.findByPiano(floor.floor());
     }
 
-    public WebAssetRepository(ZamAssetRepository assetRepository) {
+    public WebAssetController(ZamAssetRepository assetRepository) {
         this.assetRepository = assetRepository;
     }
 }
